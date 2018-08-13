66e87eda25435d9317292d83d51f40ab34b03de8
domichi ayaka <a.doumichi@solver.co.jp>
2018-08-13 14:12:45 +0900

    全体会議アプリ新規作成

---
+++ SolverMeetingApp/CardObj.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FelicaLib;
+
+namespace SolverMeetingApp
+{
+    public class CardObj
+    {
+        //private const time = 1000;
+
+        public void ReadCard()
+        {
+            Felica felica = new Felica();
+
+            while (true)
+            {
+                try
+                {
+                    felica.Polling((int)SystemCode.Any);
+
+                    var idm = felica.IDm();
+
+                    Console.Write("IDm: ");
+                    foreach (var b in idm)
+                    {
+                        Console.Write(string.Format("{0:X2}", b));
+                    }
+                    Console.Write("\r\n");
+                }
+                catch
+                {
+                }
+
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
